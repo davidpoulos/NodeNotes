e4907db949156ea3ed284e83beb244fe0ec356b8
David Poulos <davidpoulos@ymail.com>
2016-11-23 16:02:01 -0600

    Updated dependencies

--- app.js
+++ app.js
@@ -12,15 +12,21 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-
+//Initialize Routers.
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var API = require('./routes/api/index');
+var noteAPI = require('./routes/api/notes/api');
 
 var app = express();
 
 // view engine setup
 app.engine('hbs', exphbs({extname: '.hbs', defaultLayout: 'layout'}));
 app.set('view engine', 'hbs');
+
+
+//Database
+mongoose.connect('mongodb://localhost/note');
 
 //Middleware
 
@@ -29,7 +35,7 @@ app.use (
   sassMiddleware({
     src: __dirname + '/sass',
     dest: __dirname + '/public',
-    // prefix: '/stylesheets',
+    prefix: '/stylesheets',
     debug: true,
   })
 );
@@ -43,20 +49,6 @@ browserify.settings({
  //Browserify
  app.get('/javascripts/bundle.js', browserify('./client/script.js'));
 
- //Browser-Sync
-if (app.get('env') == 'development') {
-  var browserSync = require('browser-sync');
-  var config = {
-    files: ["public/**/*.{js,css}", "client/*.js", "sass/**/*.scss", "views/**/*.hbs"],
-    logLevel: 'debug',
-    logSnippet: false,
-    reloadDelay: 3000,
-    reloadOnRestart: true
-  };
-  var bs = browserSync(config);
-  app.use(require('connect-browser-sync')(bs));
-}
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -65,8 +57,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Use the routers.
 app.use('/', routes);
 app.use('/users', users);
+app.use('/api', API);
+app.use('/api/notes', noteAPI);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
