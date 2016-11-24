f3eb7e470120d2e651f27f8612e628e1a73441cc
David Poulos <davidpoulos@ymail.com>
2016-11-24 12:23:48 -0600

    Moved index page for notes back one directory

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var Note = require('../models/notes');
+
+router.get('/', function(req, res, next){
+  Note.findAsync()
+    .then(function(notes){
+      res.render('notes', { title: 'Node Notes', notes: notes});
+    })
+    .catch(next)
+    .error(console.error)
+  //res.render('notes', { title: 'Welcome to Node Notes'});
 });
 
 module.exports = router;
